2754576baa2f56ccf52b0bf4599742a1f25006ca
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-02-18 15:50:58 -0300

    Requisito 3 - ok

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
+class SolarSystem extends Component {
   render() {
     return <div data-testid='solar-system'></div>;
   }
 }
 
-export default Header;
+export default SolarSystem;
